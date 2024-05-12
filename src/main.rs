345e4c7c4e67b35e7e43fa337115a059fd56cdca
Gil Benezer <gil.benezer@gmail.com>
2024-05-11 23:56:42 -0400

    commented out Cargo.lock from .gitignore file, added addition function and test to main.rs, other files were automatically modified upon running cargo test and cargo build.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
     println!("Hello, world!");
+    println!("Two plus two equals {}", addition(2, 2))
+}
+
+fn addition(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn addition_works() {
+        let result = addition(2, 2);
+        assert_eq!(result, 4);
+    }
 }
